51e664ce6e59b1d9adc3f413efd445ba80b197d5
Leonel Viera <vieraleonel1@gmail.com>
2016-08-07 20:46:33 -0300

    - Bug fixes an improvements - Add Favourites functionality

--- www/pages/property-details/property-details.controller.js
+++ www/pages/property-details/property-details.controller.js
@@ -3,13 +3,16 @@
 
     app.controller('PropertyDetailsPageController', PropertyDetailsPageController);
 
-    PropertyDetailsPageController.$inject = ['PropertiesService']
+    PropertyDetailsPageController.$inject = ['PropertiesService', 'FavouritesService', '$timeout']
 
-    function PropertyDetailsPageController(PropertiesService) {
+    function PropertyDetailsPageController(PropertiesService, FavouritesService, $timeout) {
         var vm = this;
         
         // properties
         vm.property = {};
+
+        // methods
+        vm.toggleFav = toggleFav;
 
         activate();
         ////////////////////////
@@ -20,15 +23,40 @@
         }
 
         function prepareProperty(property) {
-            var splitTitle = property.title.split(',');
+            property.title = formatTitle(property.title);
 
-            if (splitTitle.length > 1) {
-                property.title = splitTitle[0] + ', ' + splitTitle[1];
-            } else {
-                property.title = splitTitle[0];
-            }
+            FavouritesService.isFav(property)
+                .then(function(isFav) {
+                    // workarount to force $digest cycle
+                    $timeout(function() {
+                        vm.property.isFav = isFav;
+                    }, 0)
+                });
 
             return property;
+        }
+
+        function formatTitle(title) {
+            var splitTitle = title.split(',');
+
+            if (splitTitle.length > 1) {
+                title = splitTitle[0] + ', ' + splitTitle[1];
+            } else {
+                title = splitTitle[0];
+            }
+
+            return title;
+        }
+
+        function toggleFav() {
+            if (vm.property.isFav) {
+                FavouritesService.remove(vm.property);
+            } else {
+                FavouritesService.store(vm.property);
+            }
+
+            vm.property.isFav = !vm.property.isFav;
+
         }
     }
 })(window.app);
